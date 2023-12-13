6d54d8aad3296633f1a7025a3e4ec498ce81f529
mm9al <mimi.lai.cat@gmail.com>
2023-12-14 01:38:33 +0800

    card draft

--- src/app/cardstemp/page.tsx
+++ src/app/cardstemp/page.tsx
@@ -16,6 +16,8 @@ import {
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Separator } from "@/components/ui/separator"
+import { Badge } from "@/components/ui/badge"
 
 
 import {
@@ -37,21 +39,25 @@ import {
 
 const cardstemp = () => {
   return (
-    <Card className="w-[800px] h-[700px] flex-col space-around">
-      <CardHeader className="flex-row">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <CardTitle className="px-2">
-          User Name
-          <CardDescription>
-            Institute
-          </CardDescription>
-        </CardTitle>
+    <Card className="w-[800px] h-[350px] flex-col space-around">
+      <CardHeader className="flex-row justify-between items-center">
+        <div className="flex items-center">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <CardTitle className="px-2">
+            {/* <div>User Name</div>
+            <CardDescription>Institute</CardDescription> */}
+            <div>Min Min</div>
+            <CardDescription>國立台灣大學</CardDescription>
+            <div className="text-xs font-light">1分鐘前</div>
+          </CardTitle>
+        </div>
+
         <Dialog>
           <DialogTrigger asChild>
-            <Button variant="outline">Edit Profile</Button>
+            <Button variant="outline">Edit</Button>
           </DialogTrigger>
           <DialogContent className="sm:max-w-[425px]">
             <DialogHeader>
@@ -78,22 +84,43 @@ const cardstemp = () => {
               <Button type="submit">Save changes</Button>
             </DialogFooter>
           </DialogContent>
-        </Dialog>        
+        </Dialog>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
-            <div>Location</div>
-            <div className="flex flex-col space-y-1.5">
+            {/* <div>Location</div> */}
+            <div className="flex h-5 items-center space-x-4">
+            <div className="font-bold">地點</div>
+            <Separator orientation="vertical" />
+            <Badge variant="outline">線上</Badge>
+            </div>
+
+            {/* <div className="flex flex-col space-y-1.5">
               Want To Learn:
+            </div> */}
+
+            <div className="flex h-5 items-center space-x-4">
+            <div className="font-bold">想學的技能</div>
+            <Separator orientation="vertical" />
+            <Badge variant="outline">寫前端</Badge>
             </div>
-            <div className="flex flex-col space-y-1.5">
+
+            {/* <div className="flex flex-col space-y-1.5">
               Good At:
+            </div> */}
+
+            <div className="flex h-5 items-center space-x-4">
+            <div className="font-bold">擅長的技能</div>
+            <Separator orientation="vertical" />
+            <Badge variant="outline">耍廢</Badge>
             </div>
-            <div className="flex flex-col space-y-1.5">
-              Contents:
-            </div>                        
-            <div className="flex flex-col space-y-1.5">
+
+            <div className="flex flex-col space-y-1.5 font-bold">
+              其他想說的話
+            </div>
+            <div>我不會寫前端嗚嗚嗚嗚嗚</div>
+            {/* <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Framework</Label>
               <Select>
                 <SelectTrigger id="framework">
@@ -106,23 +133,25 @@ const cardstemp = () => {
                   <SelectItem value="nuxt">Nuxt.js</SelectItem>
                 </SelectContent>
               </Select>
-            </div>
-          </div>
+            </div> */}
+      </div>
         </form>
       </CardContent>
       <CardFooter className="flex">
-        <Button variant="outline">
+        <Button variant="outline" className="mx-2">
           按讚
         </Button>
-        <Button variant="outline">
+        <Button variant="outline" className="mx-2">
           留言
         </Button>
-        <Button variant="outline">
+        <Button variant="outline" className="mx-2">
           私訊
         </Button>
           
       </CardFooter>
     </Card>
+
+    
 
   );
 };
