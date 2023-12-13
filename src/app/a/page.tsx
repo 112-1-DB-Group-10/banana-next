7890bbfa1b6d5731029515870fb778d274b3dbba
mchen127 <mchen127.p@gmail.com>
2023-12-13 21:37:41 +0800

    finish all card actions but filtering display

--- src/app/a/page.tsx
+++ src/app/a/page.tsx
@@ -1,7 +1,21 @@
 'use server';
 
-import { getHotCards, getNewestCards, searchCards, getCardsByLabel } from "@/db/cardQueries";
+import { 
+  getHotCards, 
+  getNewestCards, 
+  searchCards,
+  getCardsByLabel,
+  getCardsByTopic,
+  getCardsPostedByUser,
+  getCardsLikedOrCommentedByUser,
+  likeCard,
+  commentOnCard,
+  deleteCard,
+  updateCard,
+ } from "@/db/cardQueries";
 
+
+import {v4 as uuidv4} from 'uuid';
 
 
 export default async function HomePage() {
@@ -11,10 +25,18 @@ export default async function HomePage() {
   // const hotCards = await getHotCards();
   // const newCards = await getNewestCardsPublic(10, 2);
   // const searchedCards = await searchCards(false, "好想", 10, 2);
-  const filteredCardsByLabel = await getCardsByLabel(true, "天文學");
+  // const filteredCardsByLabel = await getCardsByLabel(true, "天文學");
+  // const filteredCardsByTopic = await getCardsByTopic(true, "體育");
+  // const cardsPostedByUser = await getCardsPostedByUser("9456173c-2dfd-4f98-9012-6573ab4f15e8");
+  // const cardsLikedOrCommentedByUser = await getCardsLikedOrCommentedByUser("ce9534bd-efe3-4187-b8a3-4b2d7da96018");
+  // await deleteCard("ada86234-12cb-40df-8ac4-b7057798da3d");
+  await updateCard("ada86234-12cb-40df-8ac4-b7057798da3d", new Date(), "I want to rest.");
+
 
   return (
     <div>
+
+
       {/* {ret.map((row, index) => (
         <div key={index}>{row.card_id}: {row.count}</div>
       ))} */}
