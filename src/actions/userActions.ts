f62b2f2d31f07ef3e27c552e66f5f0d7585b8fb6
mm9al <mimi.lai.cat@gmail.com>
2023-12-16 21:27:45 +0800

    some trash

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -8,7 +8,38 @@ import { findInstitute } from './adminActions';
 import { findSourceMap } from 'module';
 
 //透過 id 找到使用者以及他的 institute
-export const getUserById = async (user_id: any) => {
+// export const getUserById = async (user_id: any) => {
+//   try {
+//     const user = await db
+//       .select({
+//         username: usersTable.username,
+//         sex: usersTable.sex,
+//         age: usersTable.age,
+//         email: usersTable.email,
+//         role: usersTable.role,
+//         suspended: usersTable.suspended,
+//         avatar: usersTable.avatar,
+//         institute: applicationsTable.institute,
+//       })
+//       .from(usersTable)
+//       .innerJoin(applicationsTable, eq(usersTable.user_id, applicationsTable.user_id))
+//       .where(
+//         and(
+//         and(
+//           eq(applicationsTable.user_id, user_id),
+//           eq(applicationsTable.verification, 'pass'),
+//         ),
+//         eq(usersTable.user_id, user_id)
+//         )
+//         );
+//     return user;
+//   } catch (error) {
+//     console.error('Error finding user:', error);
+//     throw error;
+//   }
+// };
+
+export const getUserById = async (userId: any) => {
   try {
     const user = await db
       .select({
@@ -19,20 +50,14 @@ export const getUserById = async (user_id: any) => {
         role: usersTable.role,
         suspended: usersTable.suspended,
         avatar: usersTable.avatar,
-        institute: applicationsTable.institute,
       })
       .from(usersTable)
-      .innerJoin(applicationsTable, eq(usersTable.user_id, applicationsTable.user_id))
-      .where(
-        and(
-        and(
-          eq(applicationsTable.user_id, user_id),
-          eq(applicationsTable.verification, 'pass'),
-        ),
-        eq(usersTable.user_id, user_id)
-        )
-        );
-    return user;
+      .where(eq(usersTable.user_id, userId));
+    if (user.length > 0) {
+      return user[0];
+    } else {
+      throw Error('User Not Found.');
+    }
   } catch (error) {
     console.error('Error finding user:', error);
     throw error;
