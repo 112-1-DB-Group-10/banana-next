cd873a792f66a25fe64d1b49c0c00687168be0cb
Ruby <ruby0322@ntu.im>
2023-12-16 15:02:29 +0800

    Testing

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,5 +1,7 @@
 'use server';
 
+import { getConversations } from '@/actions/chatActions';
+import { getUserSession } from '@/lib/session';
 import UserList from './user-list';
 
 export default async function ChatLayout({
@@ -7,6 +9,9 @@ export default async function ChatLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getUserSession();
+  const conversations = await getConversations(session.user_id);
+  console.log(conversations);
   return (
     <div className="flex items-center justify-around gap-8">
       <UserList />
--- src/app/chat/user-list.tsx
+++ src/app/chat/user-list.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
-import { MdMinimize } from 'react-icons/md';
-import { usePathname, useSearchParams } from 'next/navigation';
-import Avatar from '@/components/avatar';
+import React from 'react';
 import { Card } from '@/components/ui/card';
 import { User } from '@/db/types';
 import users from '@/db/users.json';
@@ -11,42 +8,14 @@ import Search from './search';
 import UserItem from './user-item';
 
 const UserList: React.FC = () => {
-  const searchParms = useSearchParams();
-  const pathname = usePathname();
-  const [collapsed, setCollapsed] = useState<boolean>(true);
-  const query: string = searchParms.get('q') || '';
-  const filteredUsers = users
-    .filter((user) => user.user_id !== 'xdd877')
-    .filter(
-      (user) => user.username.toLowerCase().indexOf(query.toLowerCase()) !== -1,
-    );
-  if (pathname.split('/').length >= 3 && collapsed) {
-    return (
-      <Card
-        onClick={() => setCollapsed(!collapsed)}
-        className="h-fit w-fit cursor-pointer p-4 transition-all duration-100 hover:scale-105"
-      >
-        <div>
-          {users
-            .filter((user) => user.user_id === pathname.split('/')[2])
-            .map((user, index) => (
-              <Avatar key={`user-avatar-${index}`} image={user.avatar} />
-            ))}
-        </div>
-      </Card>
-    );
-  }
+  // const query: string = searchParms.get('q') || '';
+  const filteredUsers = users.filter((user) => user.user_id !== 'xdd877');
+  // .filter(
+  //   (user) => user.username.toLowerCase().indexOf(query.toLowerCase()) !== -1,
+  // );
   return (
     <Card className="duration-400 h-[30rem] w-[20rem] overflow-y-scroll p-4 transition-all">
-      <div className="flex items-center">
-        {!collapsed && (
-          <MdMinimize
-            onClick={() => setCollapsed(true)}
-            className="h-6 w-6 cursor-pointer"
-          />
-        )}
-        <Search />
-      </div>
+      <Search />
       <div className="w-full">
         {filteredUsers.map((user, index) => (
           <UserItem
