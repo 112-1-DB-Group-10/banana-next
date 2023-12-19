2fb3114ef4ee42b9f04a583c771bbbd1688b1685
b10705033 <b10705033@ntu.im>
2023-12-20 03:32:03 +0800

    search admin/users

--- src/app/admin/users/search.tsx
+++ src/app/admin/users/search.tsx
@@ -1,9 +1,41 @@
 'use client';
 
 import { Input } from '@/components/ui/input';
+import React, { useState } from 'react';
+import { MdClear } from 'react-icons/md';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
-const Search = () => {
-  return <Input className="pl-4" />;
+const Search: React.FC = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState<string>(
+    searchParams.get('q') || '',
+  );
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const q = e.target.value;
+    setSearchQuery(q);
+    if (q.length > 0) router.push(`${pathname}?q=${q}`);
+    else if (searchParams.get('q')) router.push(pathname);
+  };
+  return (
+    <div className="flex w-full items-center gap-2">
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={handleSearch}
+        className="w-full rounded-2xl border-gray-200 border-transparent px-2 py-2 focus:outline-none"
+        placeholder="要找的使用者是..."
+      />
+      {searchQuery.length > 0 && (
+        <Link onClick={() => setSearchQuery('')} href={pathname}>
+          <MdClear className="h-6 w-6" />
+        </Link>
+      )}
+    </div>
+  );
 };
+
 
 export default Search;
