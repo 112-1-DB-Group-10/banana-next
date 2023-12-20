add739245017087fcc267ecdafdb8388a54dcf55
Ruby <ruby0322@ntu.im>
2023-12-20 14:33:56 +0800

    Final Polish

--- src/app/please-login.tsx
+++ src/app/please-login.tsx
@@ -13,14 +13,14 @@ import { Button } from "@/components/ui/button";
 const PleaseLogin = () => {
     return (
       <div className="flex items-center justify-center w-[30rem] h-[45rem]">
-        <Alert>
+        <Alert variant="destructive"> 
             <Terminal className="h-4 w-4" />
                 <AlertTitle>驗證失敗</AlertTitle>
                 <AlertDescription>
                 你尚未登入，請先登入！
                 </AlertDescription>
                 <div className="flex items-center justify-end">
-                <Button asChild variant='link'>
+                <Button variant="destructive" asChild>
                     <Link href='/api/auth/signin'>
                         前往登入
                     </Link>
